51287fac5f699ad37b9e4c55b26bf8be92b94391
Rommaaan <147040323+Rommaaan@users.noreply.github.com>
2024-01-20 13:37:41 +0300

    Add files via upload

---
+++ Lab_10.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    setlocale(0, "");
+
+    int sn1 = rand() % 10 + 1;
+    int sn2 = rand() % 10 + 1;
+    int sn3 = rand() % 10 + 1;
+
+    int attempts = 5;
+    int guessedNumbers = 0;
+    int inputNumber;
+
+    cout << " [] Игра 'Угадайка' []\n\n";
+    cout << " [1] Начать игру \n";
+    cout << " [2] Выход \n\n";
+    cout << " Выбирите действие: "; cin >> inputNumber;
+
+    if (inputNumber != 1) {
+        cout << " Вы ввели неправильное число";
+    }
+    else
+
+        while (attempts > 0 && guessedNumbers < 3) {
+            cout << " Игра началась!\n";
+            cout << " Попытки: [" << attempts << " / 5]\n";
+            cout << " Введите число от 1 до 10: "; cin >> inputNumber;
+
+            if (inputNumber == sn1 and inputNumber == sn2 and inputNumber == sn3) {
+                cout << " Вы угадали число!\n";
+                guessedNumbers++;
+            }
+            else {
+                cout << " Вы не угадали число :(\n";
+                cout << "\n";
+                attempts--;
+            }
+
+        }
+
+    cout << "\n Игра окончена!\n";
+    cout << " Вы угадали " << guessedNumbers << " из 3 чисел.\n";
+
+    return 0;
+}
---
+++ Lab_11.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    cout << " [] Геометрические фигуры []\n\n";
+
+    int c;
+    int a;
+    int d;
+    char t;
+    int i = 0;
+
+    cout << " [1] Линия \n";
+    cout << " [2] Выход \n";
+    cout << " Выбирите номер: "; cin >> c;
+    cout << "\n\n";
+
+    switch (c) {
+    case 1:
+        cout << " Вы выбрали: линия\n\n";
+        cout << " [1] Горизонтальная\n";
+        cout << " [2] Вертикальная\n\n";
+        cout << " Введите число: "; cin >> a;
+        cout << "\n\n";
+
+        switch (a) {
+        case 1:
+            cout << " Вы выбрали: горизонтально \n\n";
+            cout << " Выбирите длинну линии: "; cin >> d;
+            cout << " Выбрать текстуру: "; cin >> t;
+            while (i < d) {
+                cout << t;
+                i++;
+            }
+            break;
+
+        case 2:
+            cout << " Вы выбрали: вертикально \n\n";
+            cout << " Выбирите длинну линии: "; cin >> d;
+            cout << " Выбрать текстуру: "; cin >> t;
+            while (i < d) {
+                cout << t << endl;
+                i++;
+            }
+            break;
+        }
+        break;
+    }
+    return 0;
+}
+
+
+
+
+
+
