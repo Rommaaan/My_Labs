835e54c24f067a03ee6dc73d6484d478c5ad70b9
FaRomik <147040323+Rommaaan@users.noreply.github.com>
2024-02-02 15:32:44 +0300

    Update Lab_13.cpp

--- Lab_13.cpp
+++ Lab_13.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void drawFilledSquare(int size, char texture) {}
-void drawHollowSquare(int size, char texture) {}
-
 int main() {
     setlocale(0, "");
 
@@ -52,40 +49,33 @@ int main() {
         cout << " Тип квадрата: "; cin >> type;
 
         cout << "\nВывод:\n";
-            if (type == 0) {
-                drawFilledSquare(size, texture); {
-                    for (int i = 0; i < size; ++i) {
-                        for (int j = 0; j < size; ++j) {
-                            cout << texture << " ";
-                        }
-                        cout << endl;
+        if (type == 0) {
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    cout << texture << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        else if (type == 1) {
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                        cout << texture << " ";
+                    }
+                    else {
+                        cout << "  ";
                     }
                 }
-
+                cout << endl;
             }
-
-            else if (type ==1) {
-                drawHollowSquare(size, texture); {
-                    for (int i = 0; i < size; ++i) {
-                        for (int j = 0; j < size; ++j) {
-                            if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                                cout << texture << " ";
-                            }
-                            else {
-                                cout << "  ";
-                            }
-                        }
-                        cout << endl;
-                    }
-                }
-            }
-
-            else {
-                cout << "Ошибка";
-                return 0;
-            }
+        }
+        else {
+            cout << "Ошибка";
+            return 0;
+        }
     }
-
     else {
         cout << "Ошибка";
         return 0;
